8b41b013fc6f7e41b5dcd6fc19eb6d9478f95bee
flypie <flypie@aliyun.com>
2017-02-16 22:08:55 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'app'
-    }
+  export default {
+    name: 'app'
+  }
 </script>
 <style>
-    html, body, #app {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-    }
+  html, body, #app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  .picker-item {
+    width: 300px;
+  }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,104 +1,104 @@
 <template>
-    <div class="main-content">
-        <ul class="list">
-            <li class="item" v-for="item in list">
-                <a :href="item.url" target="_blank" class="name">{{item.name}}</a>
-                <p class="description">{{item.description}}</p>
-                <p class="bottom">
-                    language:<span class="language">{{item.language}}</span>
-                    star:<span class="star">{{item.star}}</span>
-                </p>
-            </li>
-        </ul>
-        <mt-picker class="picker" :slots="slots" @change="onValuesChange"></mt-picker>
-    </div>
+  <div class="main-content">
+    <ul class="list">
+      <li class="item" v-for="item in list">
+        <a :href="item.url" target="_blank" class="name">{{item.name}}</a>
+        <p class="description">{{item.description}}</p>
+        <p class="bottom">
+          language:<span class="language">{{item.language}}</span>
+          star:<span class="star">{{item.star}}</span>
+        </p>
+      </li>
+    </ul>
+    <mt-picker class="picker" :slots="slots" @change="onValuesChange"></mt-picker>
+  </div>
 </template>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .main-content {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        background-color: #f8f8f8;
-    }
+  .main-content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: #f8f8f8;
+  }
 
-    .list {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        flex: 3;
-        overflow: scroll;
-    }
+  .list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    flex: 3;
+    overflow: scroll;
+  }
 
-    .list .item {
-        font-size: 14px;
-        text-align: center;
-        height: 120px;
-        padding-top: 10px;
-        border-bottom: 3px solid #fff;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-around;
-    }
+  .list .item {
+    font-size: 14px;
+    text-align: center;
+    height: 120px;
+    padding-top: 10px;
+    border-bottom: 3px solid #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
 
-    .list .item .description {
-        font-size: 10px;
-        padding: 0 10px;
-    }
+  .list .item .description {
+    font-size: 10px;
+    padding: 0 10px;
+  }
 
-    .list .item .name {
-        padding-top: 6px;
-    }
+  .list .item .name {
+    padding-top: 6px;
+  }
 
-    .list .item .language {
-        margin-right: 28px;
-    }
+  .list .item .language {
+    margin-right: 28px;
+  }
 
-    .picker {
-        overflow: hidden;
-        flex: 1;
-        background-color: #fff;
-        border-top: 2px solid #ddd;
-        box-shadow: 0 -1px 1px 1px #bbb;
-    }
+  .picker {
+    overflow: hidden;
+    flex: 1;
+    background-color: #fff;
+    border-top: 2px solid #ddd;
+    box-shadow: 0 -1px 1px 1px #bbb;
+  }
 </style>
 <script>
-    import {Indicator} from 'mint-ui'
-    import api from '../api'
-    export default {
-        name: 'hello',
-        data () {
-            return {
-                slots: [
-                    {
-                        values: [],
-                        className: 'slot1',
-                        textAlign: 'center'
-                    }
-                ],
-                list: []
-            }
-        },
-        methods: {
-            onValuesChange(picker, values) {
-                let language = values[0]
-                if (language == 'C++') {
-                    language = 'Cpp'
-                }
-                Indicator.open('加载中...');
-                api.getList(language).then(list => {
-                    this.list = list
-                    Indicator.close()
-                })
-            }
-        },
-        created: function () {
-            api.getLanguages().then(languages => {
-                this.slots[0].values = languages
-            })
+  import {Indicator} from 'mint-ui'
+  import api from '../api'
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        slots: [
+          {
+            values: [],
+            className: 'slot1',
+            textAlign: 'center'
+          }
+        ],
+        list: []
+      }
+    },
+    methods: {
+      onValuesChange(picker, values) {
+        let language = values[0]
+        if (language == 'C++') {
+          language = 'Cpp'
         }
+        Indicator.open('加载中...');
+        api.getList(language).then(list => {
+          this.list = list
+          Indicator.close()
+        })
+      }
+    },
+    created: function () {
+      api.getLanguages().then(languages => {
+        this.slots[0].values = languages
+      })
     }
+  }
 </script>
